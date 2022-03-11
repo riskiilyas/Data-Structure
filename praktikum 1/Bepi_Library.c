016de5e0cfe0bc3cf74e8a42e4eddd6edca0b66f
riskiilyas <riskiilyas03@gmail.com>
2022-03-11 19:02:06 +0700

    latihan binary tree

--- praktikum 1/Bepi_Library.c
+++ praktikum 1/Bepi_Library.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
-
+#include <string.h>
+#include <ctype.h>
 /* Struktur Node */
 
 typedef struct dnode_t {
-    char *title;
+    char* title;
     float price;
     struct dnode_t      \
         *next,
@@ -30,6 +31,7 @@ bool deq_isEmpty(Deque *deque);
 void deq_pushBack(Deque *deque, char* title, float price);
 void deq_popFront(Deque *deque);
 void deq_popBack(Deque *deque);
+void print_book(DListNode *book);
 
 /* Definisi Fungsi */
 
@@ -99,13 +101,13 @@ void deq_popFront(Deque *deque)
         if (deque->_head == deque->_tail) {
             deque->_head = NULL;
             deque->_tail = NULL;
-            print_book(&temp);
+            print_book(temp);
             free(temp);
         }
         else {
             deque->_head = deque->_head->next;
             deque->_head->prev = NULL;
-            print_book(&temp);
+            print_book(temp);
             free(temp);
         }
         deque->_size--;
@@ -117,17 +119,17 @@ void print_book(DListNode *book) {
     printf("Rent Price\t: $%f/day\n", book->price);
 }
 
-void findByTitle(Deque *deque, char *title){
+void findByTitle(Deque *deque, char* title){
     DListNode *temp = deque->_head;
     int span = strlen(title);
     while(temp!=NULL){
         int flag = 0;
         for(int i=0; i<span; i++){
-            if(title[i].tolower()!=(temp->title)[i].tolower()) break;
+            if(tolower(title[i])!= tolower(temp->title[i])) break;
             if(i==span-1) flag =1;
         }
         if(flag){
-            print_book(&temp);
+            print_book(temp);
             return;
         }
         temp = temp->next;
@@ -140,7 +142,7 @@ void findByLocation(Deque *deque, int loc){
     int now = 0;
     while(temp!=NULL){
         if(loc==now) {
-            print_book(&temp);
+            print_book(temp);
             return;
         }
         temp = temp->next;
@@ -169,14 +171,75 @@ void deq_popBack(Deque *deque)
     }
 }
 
+void pushByLoc(Deque *deque, int loc, char* title, float price){
+    int size = deque->_size;
+    DListNode *newNode = __dlist_createNode(title, price);
+    DListNode *temp = deque->_head;
+    
+    for(int i=0; i<=size; i++){    
+        if(deq_isEmpty(deque)) {
+            deque->_head = newNode;
+            deque->_tail = newNode;
+        }
+        else if(i==size)  {
+            deque->_tail->next = newNode;
+            newNode->prev = deque->_tail;
+            deque->_tail = newNode;
+        }
+        else if(i==loc){
+            if(i==0) {
+                deque->_head->prev = newNode;
+                newNode->next = deque->_head;
+                deque->_head = newNode;
+            } else {
+                temp->prev->next = newNode;
+                newNode->prev = temp->prev;
+                newNode->next = temp;
+                temp->prev = newNode;
+            }
+        }
+        temp = temp->next;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     Deque mydeq;
     deq_init(&mydeq);
 
     char inp[500];
-    while(fgets(inp, 500, stdin) != NULL) {
+    while(scanf("%[^\n]%*c", inp) != EOF) {
+        char *token = strtok(inp, ",");
+        int *loc = NULL;
+        char *title = NULL;
+        float *price = NULL;
+        int ctr = 0;
+        char empty[] = "";
+        while(token != NULL){
+            if(strcmp(token, empty)!=0 && ctr==0) {
+                int tmp = atoi(token);
+                loc = &tmp;
+            } else if (strcmp(token, empty)!=0 && ctr==1){
+                strcpy(token, title);
+            } else if (strcmp(token, empty)!=0 && ctr==2){
+                float tmp = atof(token);
+                price = &tmp;
+            }
+            ctr++;
+            token = strtok(NULL, ",");
+        }
         
+        if (loc!=NULL && title!=NULL && price!=NULL){
+            pushByLoc(&mydeq, *loc, title, *price);
+        } else if (*loc==-1){
+            deq_popBack(&mydeq);
+        } else if (loc!=NULL){
+            findByLocation(&mydeq, *loc);
+        } else if (title!=NULL){
+            findByTitle(&mydeq, title);
+        } else {
+            deq_popFront(&mydeq);
+        }
     }
 
     printf("Bepi has %d book(s).", mydeq._size);
